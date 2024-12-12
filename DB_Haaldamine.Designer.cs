c98fa73125a65d4d1e5f7828aa340b33bae5b7f7
Anton Ivanov <anton9032@gmail.com>
2024-12-12 19:45:18 +0200

    upd

--- DB_Haaldamine.Designer.cs
+++ DB_Haaldamine.Designer.cs
@@ -28,62 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tableListBox = new System.Windows.Forms.ListBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
+            this.uuenda_btn = new System.Windows.Forms.Button();
+            this.kustuta_btn = new System.Windows.Forms.Button();
+            this.lisa_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // listBox1
+            // tableListBox
             // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(12, 139);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(221, 379);
-            this.listBox1.TabIndex = 0;
+            this.tableListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.tableListBox.FormattingEnabled = true;
+            this.tableListBox.ItemHeight = 25;
+            this.tableListBox.Location = new System.Drawing.Point(12, 59);
+            this.tableListBox.Name = "tableListBox";
+            this.tableListBox.Size = new System.Drawing.Size(202, 229);
+            this.tableListBox.TabIndex = 0;
+            this.tableListBox.DoubleClick += new System.EventHandler(this.vali_btn_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(255, 331);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 318);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(755, 190);
+            this.dataGridView1.Size = new System.Drawing.Size(819, 203);
             this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(255, 12);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(231, 59);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(755, 308);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(367, 200);
             this.flowLayoutPanel1.TabIndex = 21;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Location = new System.Drawing.Point(82, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 26);
+            this.label1.Size = new System.Drawing.Size(82, 26);
             this.label1.TabIndex = 22;
-            this.label1.Text = "Tabel";
+            this.label1.Text = "Tabelid";
+            // 
+            // uuenda_btn
+            // 
+            this.uuenda_btn.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uuenda_btn.Location = new System.Drawing.Point(438, 278);
+            this.uuenda_btn.Name = "uuenda_btn";
+            this.uuenda_btn.Size = new System.Drawing.Size(81, 24);
+            this.uuenda_btn.TabIndex = 25;
+            this.uuenda_btn.Text = "Uuenda";
+            this.uuenda_btn.UseVisualStyleBackColor = true;
+            // 
+            // kustuta_btn
+            // 
+            this.kustuta_btn.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kustuta_btn.Location = new System.Drawing.Point(352, 278);
+            this.kustuta_btn.Name = "kustuta_btn";
+            this.kustuta_btn.Size = new System.Drawing.Size(80, 24);
+            this.kustuta_btn.TabIndex = 24;
+            this.kustuta_btn.Text = "Kustuta";
+            this.kustuta_btn.UseVisualStyleBackColor = true;
+            // 
+            // lisa_btn
+            // 
+            this.lisa_btn.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lisa_btn.Location = new System.Drawing.Point(231, 278);
+            this.lisa_btn.Name = "lisa_btn";
+            this.lisa_btn.Size = new System.Drawing.Size(115, 24);
+            this.lisa_btn.TabIndex = 23;
+            this.lisa_btn.Text = "Lisa andmed";
+            this.lisa_btn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(604, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 267);
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
             // 
             // DB_Haaldamine
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1022, 533);
+            this.ClientSize = new System.Drawing.Size(844, 533);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.uuenda_btn);
+            this.Controls.Add(this.kustuta_btn);
+            this.Controls.Add(this.lisa_btn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tableListBox);
             this.Name = "DB_Haaldamine";
             this.Text = "Andmebaasi haaldamine";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,9 +141,13 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox tableListBox;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button uuenda_btn;
+        private System.Windows.Forms.Button kustuta_btn;
+        private System.Windows.Forms.Button lisa_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- DB_Haaldamine.cs
+++ DB_Haaldamine.cs
@@ -14,32 +14,192 @@ namespace Kino
 {
     public partial class DB_Haaldamine : Form
     {
+        DatabaseHelper dbHelper = new DatabaseHelper();
         static string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\"));
-        static string db_name = "Andmebaas.mdf";
-        static string db_path = Path.Combine(projectRoot, "Andmebaas.mdf");
-        static string imageFolder = Path.Combine(projectRoot, @"Pildid");
+        static string imageFolder = Path.Combine(projectRoot, @"Posters");
 
-        SqlConnection conn;
-        SqlCommand cmd;
-        SqlDataAdapter adapter;
+        //SqlConnection conn;
+        //SqlCommand cmd;
+        //SqlDataAdapter adapter;
 
-        OpenFileDialog open;
-        SaveFileDialog save;
-        Form popupForm;
-        DataTable laotable;
-        string extension;
-        private byte[] imageData;
+        //OpenFileDialog open;
+        //SaveFileDialog save;
+        //Form popupForm;
+        //DataTable laotable;
+        //string extension;
+        //private byte[] imageData;
 
         public DB_Haaldamine()
         {
             InitializeComponent();
+            LoadTableNames();
         }
 
-        public void FindDB()
+        private void LoadTableNames()
         {
-            if (File.Exists(db_path))
+            string query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES " +
+                            "WHERE TABLE_TYPE = 'BASE TABLE'";
+            DataTable tables = dbHelper.ExecuteQuery(query);
+            if (tables != null)
             {
-                conn = new SqlConnection($@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={db_path};Integrated Security=True");
+                foreach (DataRow row in tables.Rows)
+                {
+                    tableListBox.Items.Add(row["TABLE_NAME"].ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка при загрузке таблиц.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void vali_btn_Click(object sender, EventArgs e)
+        {
+            if (tableListBox.SelectedItem != null)
+            {
+                string selected_table = tableListBox.SelectedItem.ToString();
+
+                try
+                {
+                    string query = $"SELECT * FROM {selected_table}";
+                    DataTable dataTable = dbHelper.ExecuteQuery(query);
+                    dataGridView1.DataSource = dataTable;
+                    CreateInputFieldsForTable(selected_table);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Viga andmete laadimisel: {ex.Message}", "Viga", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        public void CreateInputFieldsForTable(string tableName)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown; 
+            flowLayoutPanel1.WrapContents = false;
+            flowLayoutPanel1.AutoScroll = true;
+
+            string query = $"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{tableName}'";
+            DataTable dataTable = dbHelper.ExecuteQuery(query);
+
+            Font labelFont = new Font("Microsoft Sans Serif", 13f);
+            Font fieldFont = new Font("Microsoft Sans Serif", 9.5f);
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                string columnName = dataTable.Rows[i]["COLUMN_NAME"].ToString();
+                string dataType = dataTable.Rows[i]["DATA_TYPE"].ToString();
+                if (i == 0)
+                    continue;
+
+                string labelText = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(columnName.Replace("_", " "));
+                Panel panel = new Panel();
+                panel.Width = flowLayoutPanel1.Width - 10;
+                panel.Height = 30;
+
+                Label label = new Label();
+                label.Text = labelText;
+                label.Font = labelFont;
+                label.AutoSize = false;
+                label.Width = 120;
+                label.TextAlign = ContentAlignment.MiddleLeft;
+
+                Control inputField;
+                if (dataType == "int" || dataType == "decimal" || dataType == "float")
+                {
+                    NumericUpDown numericUpDown = new NumericUpDown();
+                    numericUpDown.Name = columnName;
+                    numericUpDown.Width = 60;
+                    numericUpDown.Height = 40;
+                    numericUpDown.Font = fieldFont;
+                    numericUpDown.Maximum = decimal.MaxValue;
+                    inputField = numericUpDown;
+                }
+                else if (dataType == "datetime" || dataType == "date")
+                {
+                    DateTimePicker dateTimePicker = new DateTimePicker();
+                    dateTimePicker.Name = columnName;
+                    dateTimePicker.Format = DateTimePickerFormat.Custom;
+                    dateTimePicker.CustomFormat = "yyyy-MM-dd HH:mm:ss"; 
+                    dateTimePicker.Width = 160;
+                    dateTimePicker.Font = fieldFont;
+                    inputField = dateTimePicker;
+                }
+                else
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Name = columnName;
+                    textBox.Width = 100;
+                    textBox.Font = fieldFont;
+                    inputField = textBox;
+                }
+
+                inputField.Margin = new Padding(0, 15, 0, 0);
+                inputField.Location = new Point(label.Width + 10, 0);
+                panel.Controls.Add(inputField);
+                panel.Controls.Add(label);
+                flowLayoutPanel1.Controls.Add(panel);
+            }
+        }
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+            foreach (DataGridViewCell cell in selectedRow.Cells)
+            {
+                string columnName = cell.OwningColumn.Name;
+                Control[] controls = flowLayoutPanel1.Controls.Find(columnName, true);
+                if (controls.Length > 0)
+                {
+                    Control inputField = controls[0];
+                    if (inputField is TextBox textBox)
+                    {
+                        textBox.Text = cell.Value?.ToString() ?? string.Empty;
+                    }
+                    else if (inputField is NumericUpDown numericUpDown)
+                    {
+                        if (decimal.TryParse(cell.Value?.ToString(), out decimal numericValue))
+                        {
+                            numericUpDown.Value = numericValue;
+                        }
+                        else
+                        {
+                            numericUpDown.Value = 0;
+                        }
+                    }
+                    else if (inputField is DateTimePicker dateTimePicker)
+                    {
+                        if (DateTime.TryParse(cell.Value?.ToString(), out DateTime dateValue))
+                        {
+                            dateTimePicker.Value = dateValue;
+                        }
+                        else
+                        {
+                            dateTimePicker.Value = DateTime.Now;
+                        }
+                    }
+                }
+            }
+
+            // Обработка изображения (если присутствует)
+            if (dataGridView1.Columns.Contains("poster"))
+            {
+                // Обработка изображения
+                if (selectedRow.Cells["poster"].Value != null && !string.IsNullOrEmpty(selectedRow.Cells["poster"].Value.ToString()))
+                {
+                    try
+                    {
+                        pictureBox1.Image = Image.FromFile(Path.Combine(imageFolder, selectedRow.Cells["poster"].Value.ToString()));
+                        pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при загрузке изображения: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    pictureBox1.Image = null;
+                }
             }
         }
     }
